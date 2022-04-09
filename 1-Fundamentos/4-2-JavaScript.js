3d492aa5a1e5e0084f837d0aa3785b6ba2b8c60a
Marcelo Marques <marcelo-mls@hotmail.com>
2022-04-08 23:59:40 -0300

    Adiciona ex 02

---
+++ 1-Fundamentos/4-2-JavaScript.js
@@ -0,0 +1,23 @@
+// Gerar lista com 10 numeros aleatórios
+sorteio = []
+for (let i = 0; i < 10; i=i+1) {
+    sorteio.push(Math.floor(Math.random()*101))
+}
+console.log("|------ NÚMEROS SORTEADOS -----|")
+console.log(sorteio)
+
+// Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log();
+console.log("")
+console.log("1) todos os números")
+for(let num of sorteio) {
+    console.log(num)
+}
+
+// Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+soma = 0
+for(let num of sorteio){
+    soma = soma + num
+}
+console.log("")
+console.log("2) soma")
+console.log(soma)
