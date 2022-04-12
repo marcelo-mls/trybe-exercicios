d64fbeb2599938b7a28285f5213a4ecea6267963
Marcelo Marques <marcelo-mls@hotmail.com>
2022-04-11 23:40:47 -0300

    Adiciona nova versão da função de Indice

--- 1-Fundamentos/4-4-JavaScript-B.js
+++ 1-Fundamentos/4-4-JavaScript-B.js
@@ -17,7 +17,7 @@ Retorno esperado: true
 verificaPalindrome('desenvolvimento') 
 Retorno esperado: false */
 
-/* function palindromo(word){
+function palindromo(word){
     let palavra = word
     let palavraInvertida = palavra.split("").reverse().join("")
 
@@ -30,7 +30,7 @@ Retorno esperado: false */
 
 console.log("")
 console.log("1) Palíndromo")
-console.log( palindromo("Hannah") ) */
+console.log( palindromo("Hannah") )
 
 // 2 - Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
 
@@ -49,9 +49,35 @@ console.log("")
 console.log("2) Indice do Maior")
 console.log(indiceMaior(arrayNums(6, 100)))
 
-
-
 // 3 - Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
+let array = [6,4,3,5,7,2]
+
+function indiceMaiorMenor(array, maiorMenor){
+    
+    let maior = array[0]
+    let menor = array[0]
+    for(let n of array){
+        if(n > maior){
+            maior = n
+        }
+        if (n < menor){
+            menor = n
+        }
+    }
+    if(maiorMenor == "maior"){
+        return "O array é: " + array + ". O MAIOR número é " + maior + ". E o seu indice é: " + array.indexOf(maior)
+    } else if (maiorMenor == "menor") {
+        return "O array é: " + array + ". O MENOR número é " + menor + ". E o seu indice é: " + array.indexOf(menor)
+    } else if (maiorMenor == "dois") {
+        return "O array é: " + array + ". O MAIOR e o MENOR número são " + maior + " e " + menor + ". E o seus indices são: " + array.indexOf(maior) + " e " + array.indexOf(menor) + ", respectivamente."
+    } else {
+        return "Parametro invalido, informe: MAIOR, MENOR ou DOIS"
+    }
+}
+
+console.log("")
+console.log("3) Indice do Menor")
+console.log(indiceMaiorMenor(arrayNums(6, 100), "dois") )
 
 
 /*  4 - Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
